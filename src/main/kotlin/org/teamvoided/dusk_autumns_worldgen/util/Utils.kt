512a13e7788b7d5c870acb9143952e05898d3381
theendercore <theendercore@gmail.com>
2024-02-14 21:25:53 +0200

    - added .add to lists

--- src/main/kotlin/org/teamvoided/dusk_autumns_worldgen/util/Utils.kt
+++ src/main/kotlin/org/teamvoided/dusk_autumns_worldgen/util/Utils.kt
@@ -5,4 +5,10 @@ import net.minecraft.loot.provider.number.UniformLootNumberProvider
 object Utils {
     fun uniformNum(x: Number, y: Number): UniformLootNumberProvider =
         UniformLootNumberProvider.create(x.toFloat(), y.toFloat())
+
+    fun <T> List<T>.add(vararg item: T): List<T> {
+        val x = this.toMutableList()
+        x.addAll(item)
+        return x.toList()
+    }
 }
