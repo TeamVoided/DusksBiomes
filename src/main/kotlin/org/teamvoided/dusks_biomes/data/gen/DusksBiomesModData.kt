895541f816d59dbeb780010c9a1dff3daeffda7e
theendercore <theendercore@gmail.com>
2024-07-18 18:08:57 +0300

    - :(

--- src/main/kotlin/org/teamvoided/dusks_biomes/data/gen/DusksBiomesModData.kt
+++ src/main/kotlin/org/teamvoided/dusks_biomes/data/gen/DusksBiomesModData.kt
@@ -4,8 +4,8 @@ import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
 import net.minecraft.registry.RegistryKeys
 import net.minecraft.registry.RegistrySetBuilder
-import org.teamvoided.dusk_autumns_worldgen.data.gen.world.gen.ConfiguredFeatureCreator
 import org.teamvoided.dusks_biomes.DusksBiomesMod.log
+import org.teamvoided.dusks_biomes.data.gen.*
 import org.teamvoided.dusks_biomes.data.gen.structure.StructureFeatureCreator
 import org.teamvoided.dusks_biomes.data.gen.structure.StructurePoolCreator
 import org.teamvoided.dusks_biomes.data.gen.structure.StructureProcessorCreator
