01aa0a9ce74f05a3071e381e81ef001b6a31b94b
TheEnderCore <theendercore@gmail.com>
2025-10-28 14:34:06 +0200

    sus sand attempt 1

--- src/main/kotlin/org/teamvoided/dusks_biomes/data/gen/structure/processor_list/OceanRuins.kt
+++ src/main/kotlin/org/teamvoided/dusks_biomes/data/gen/structure/processor_list/OceanRuins.kt
@@ -3,9 +3,13 @@ package org.teamvoided.dusks_biomes.data.gen.structure.processor_list
 import dev.worldgen.lithostitched.worldgen.processor.BlockSwapStructureProcessor
 import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.data.worldgen.BootstrapContext
+import net.minecraft.util.valueproviders.ConstantInt
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
-import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessorList
+import net.minecraft.world.level.levelgen.structure.templatesystem.*
+import net.minecraft.world.level.levelgen.structure.templatesystem.rule.blockentity.AppendLoot
+import net.minecraft.world.level.storage.loot.BuiltInLootTables
+import org.teamvoided.dusks_biomes.data.gen.structure.StructureProcessorCreator.RuleProcessor
 import org.teamvoided.dusks_biomes.data.gen.structure.StructureProcessorCreator.register
 import org.teamvoided.dusks_biomes.data.structure.DuskStructureProcessorLists.OCEAN_RUIN_WARM_RED
 
@@ -13,13 +17,26 @@ import org.teamvoided.dusks_biomes.data.structure.DuskStructureProcessorLists.OC
 object OceanRuins {
     fun BootstrapContext<StructureProcessorList>.oceanRuinWarmRed() {
         register(
-            OCEAN_RUIN_WARM_RED, blockSwap(
+            OCEAN_RUIN_WARM_RED,
+            CappedProcessor(
+                RuleProcessor(
+                    ProcessorRule(
+                        BlockMatchTest(Blocks.RED_SAND),
+                        AlwaysTrueTest.INSTANCE,
+                        PosAlwaysTrueTest.INSTANCE,
+                        Blocks.SUSPICIOUS_SAND.defaultBlockState(),
+                        AppendLoot(BuiltInLootTables.OCEAN_RUIN_WARM_ARCHAEOLOGY)
+                    )
+                ), ConstantInt.of(2)
+            ),
+            blockSwap(
+                Blocks.SUSPICIOUS_SAND to Blocks.SUSPICIOUS_SAND,
+                Blocks.SAND to Blocks.RED_SAND,
                 Blocks.SANDSTONE to Blocks.RED_SANDSTONE,
                 Blocks.CUT_SANDSTONE to Blocks.CUT_RED_SANDSTONE,
                 Blocks.CHISELED_SANDSTONE to Blocks.CHISELED_RED_SANDSTONE,
                 Blocks.SANDSTONE_STAIRS to Blocks.RED_SANDSTONE_STAIRS,
-                Blocks.SAND to Blocks.RED_SAND,
-            )
+            ),
         )
     }
 
