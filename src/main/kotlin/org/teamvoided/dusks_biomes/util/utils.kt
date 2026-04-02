1e811d532893feb019e815c95179e29518c45c9b
TheEnderCore <theendercore@gmail.com>
2026-04-02 23:45:30 +0300

    creative works is back

--- src/main/kotlin/org/teamvoided/dusks_biomes/util/utils.kt
+++
@@ -1,9 +0,0 @@
-package org.teamvoided.dusks_biomes.util
-
-import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction
-import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator
-
-object Utils {
-    fun setCount(x: Number, y: Number) = SetItemCountFunction.setCount(uniformNum(x, y))
-    fun uniformNum(x: Number, y: Number) = UniformGenerator.between(x.toFloat(), y.toFloat())
-}
