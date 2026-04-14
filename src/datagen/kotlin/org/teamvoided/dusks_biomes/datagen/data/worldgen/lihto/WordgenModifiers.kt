aa813c7e96f6fe4a14b9f7cc5f527c1f6308d978
TheEnderCore <theendercore@gmail.com>
2026-04-15 01:44:01 +0300

    dategen for all biome modifiers

--- src/datagen/kotlin/org/teamvoided/dusks_biomes/datagen/data/worldgen/lihto/WordgenModifiers.kt
+++ src/datagen/kotlin/org/teamvoided/dusks_biomes/datagen/data/worldgen/lihto/WordgenModifiers.kt
@@ -1,11 +1,18 @@
 package org.teamvoided.dusks_biomes.datagen.data.worldgen.lihto
 
 import dev.worldgen.lithostitched.api.worldgen.modifier.WorldgenModifier
+import dev.worldgen.lithostitched.api.worldgen.util.BiomeEffects
+import net.minecraft.core.HolderSet
 import net.minecraft.core.registries.Registries
 import net.minecraft.data.worldgen.BootstrapContext
+import net.minecraft.world.level.biome.Biomes
+import net.minecraft.world.level.levelgen.structure.BuiltinStructureSets
+import net.minecraft.world.level.levelgen.structure.StructureSet
+import org.teamvoided.dusks_biomes.data.structure.DuskStructureFeatures
 import org.teamvoided.dusks_biomes.data.structure.DuskStructureProcessorLists
 import org.teamvoided.dusks_biomes.data.tags.DuskPoolTags
 import org.teamvoided.dusks_biomes.data.world.gen.litho.DuskWorldgenModifiers
+import java.util.*
 
 object WordgenModifiers {
 
@@ -14,15 +21,71 @@ object WordgenModifiers {
     fun BootstrapContext<WorldgenModifier>.boostrap() {
         val templatePools = lookup(Registries.TEMPLATE_POOL)
         val processorLists = lookup(Registries.PROCESSOR_LIST)
+        val structureSets = lookup(Registries.STRUCTURE_SET)
+        val structures = lookup(Registries.STRUCTURE)
+        val biomes = lookup(Registries.BIOME)
 
         register(
             DuskWorldgenModifiers.TRIAL_CHAMBERS_FIX,
-            WorldgenModifier.builder()
-                .setPoolElementProcessors(
-                    templatePools.getOrThrow(DuskPoolTags.TRIAL_CHAIN_FIX),
-                    processorLists.getOrThrow(DuskStructureProcessorLists.TRIAL_CHAMBERS_FIX),
-                    true
+            WorldgenModifier.builder().setPoolElementProcessors(
+                templatePools.getOrThrow(DuskPoolTags.TRIAL_CHAIN_FIX),
+                processorLists.getOrThrow(DuskStructureProcessorLists.TRIAL_CHAMBERS_FIX),
+                true
+            )
+        )
+
+        register(
+            DuskWorldgenModifiers.ADD_RUIN_WARM_RED,
+            WorldgenModifier.builder().addStructureSetEntries(
+                structureSets.getOrThrow(BuiltinStructureSets.OCEAN_RUINS),
+                StructureSet.StructureSelectionEntry(
+                    structures.getOrThrow(DuskStructureFeatures.OCEAN_RUIN_WARM_RED),
+                    1
                 )
+            )
+        )
+
+        register(
+            DuskWorldgenModifiers.ADD_VILLAGES,
+            WorldgenModifier.builder().addStructureSetEntries(
+                structureSets.getOrThrow(BuiltinStructureSets.OCEAN_RUINS),
+                StructureSet.StructureSelectionEntry(
+                    structures.getOrThrow(DuskStructureFeatures.SWAMP_VILLAGE),
+                    1
+                ),
+                StructureSet.StructureSelectionEntry(
+                    structures.getOrThrow(DuskStructureFeatures.MANGROVE_SWAMP_VILLAGE),
+                    1
+                )
+            )
+        )
+
+        register(
+            DuskWorldgenModifiers.ADJUST_DESERT_COLORS,
+            WorldgenModifier.builder().replaceEffects(
+                HolderSet.direct(
+                    biomes.getOrThrow(Biomes.DESERT),
+                    biomes.getOrThrow(Biomes.BADLANDS),
+                    biomes.getOrThrow(Biomes.ERODED_BADLANDS),
+                    biomes.getOrThrow(Biomes.WOODED_BADLANDS),
+                ),
+                BiomeEffects(
+                    Optional.empty(),
+                    Optional.of(4445678),
+                    Optional.of(270131),
+                    Optional.empty(),
+                    Optional.empty(),
+                    Optional.empty(),
+                    Optional.empty(),
+                    Optional.empty(),
+                    Optional.empty(),
+                    Optional.empty(),
+                    Optional.empty(),
+                    Optional.empty(),
+                    Optional.empty(),
+                    Optional.empty(),
+                )
+            )
         )
     }
 }
--- src/main/kotlin/org/teamvoided/dusks_biomes/data/world/gen/litho/DuskWorldgenModifiers.kt
+++ src/main/kotlin/org/teamvoided/dusks_biomes/data/world/gen/litho/DuskWorldgenModifiers.kt
@@ -7,6 +7,9 @@ import org.teamvoided.dusks_biomes.util.key
 object DuskWorldgenModifiers {
 
     val TRIAL_CHAMBERS_FIX = key("trial_chambers_fix")
+    val ADD_RUIN_WARM_RED = key("add_ruin_warm_red")
+    val ADD_VILLAGES = key("add_villages")
+    val ADJUST_DESERT_COLORS = key("adjust_desert_colors")
 
     fun key(id: String) = LithostitchedRegistries.WORLDGEN_MODIFIER.key(DusksBiomes.id(id))
 
