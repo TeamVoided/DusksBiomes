e9dc3fb6eb59906e2706c118eb4284ec6ed51f16
TheEnderCore <theendercore@gmail.com>
2026-04-15 03:43:20 +0300

    parched in chambers

--- src/datagen/kotlin/org/teamvoided/dusks_biomes/datagen/data/tags/PoolTagsProvider.kt
+++ src/datagen/kotlin/org/teamvoided/dusks_biomes/datagen/data/tags/PoolTagsProvider.kt
@@ -1,10 +1,12 @@
 package org.teamvoided.dusks_biomes.datagen.data.tags
 
+import dev.worldgen.lithostitched.api.tag.LithostitchedTemplatePoolTags
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider
 import net.minecraft.core.HolderLookup
 import net.minecraft.core.registries.Registries
 import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool
+import org.teamvoided.dusks_biomes.DusksBiomes.id
 import org.teamvoided.dusks_biomes.DusksBiomes.mc
 import org.teamvoided.dusks_biomes.data.tags.DuskPoolTags
 import org.teamvoided.dusks_biomes.util.key
@@ -23,5 +25,13 @@ class PoolTagsProvider(o: FabricDataOutput, r: CompletableFuture<HolderLookup.Pr
                 ).map { Registries.TEMPLATE_POOL.key(mc(it)) }
             )
 
+        builder(LithostitchedTemplatePoolTags.TRIAL_SPAWNER_RANGED)
+            .addOptional(key("trial_chambers/spawner/ranged/parched"))
+        builder(LithostitchedTemplatePoolTags.TRIAL_SPAWNER_SLOW_RANGED)
+            .addOptional(key("trial_chambers/spawner/slow_ranged/parched"))
+
     }
+
+    fun key(id: String) = Registries.TEMPLATE_POOL.key(id(id))
+
 }
